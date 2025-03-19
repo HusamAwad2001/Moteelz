e81cc032bddc05f2a54efc676407e5abedce6fd1
Husam Dahliz <husamdahliz@gmail.com>
2025-03-19 17:25:15 +0200

    Add wallet data models with JSON serialization support

---
+++ lib/features/wallet/data/models/wallet_category.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wallet_category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WalletCategory _$WalletCategoryFromJson(Map<String, dynamic> json) =>
+    WalletCategory(
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$WalletCategoryToJson(WalletCategory instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
---
+++ lib/features/wallet/data/models/wallet_days.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wallet_days.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WalletDays _$WalletDaysFromJson(Map<String, dynamic> json) => WalletDays(
+      days: json['days'] as String?,
+      expiryDays: json['expiry_days'] as String?,
+      expiryDate: json['expiry_date'] as String?,
+    );
+
+Map<String, dynamic> _$WalletDaysToJson(WalletDays instance) =>
+    <String, dynamic>{
+      'days': instance.days,
+      'expiry_days': instance.expiryDays,
+      'expiry_date': instance.expiryDate,
+    };
