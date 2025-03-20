263b71d7d7aa0fdb56567f57d5917e2ce3fb1580
Husam Dahliz <husamdahliz@gmail.com>
2025-03-20 10:38:18 +0200

    Add countries endpoint and repository method for wallet feature

--- lib/features/wallet/data/repos/wallets_repo_impl.dart
+++ lib/features/wallet/data/repos/wallets_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:moteelz/core/networking/api/api_consumer.dart';
 import 'package:moteelz/core/networking/api/end_points.dart';
 import 'package:moteelz/core/networking/errors/api_error_handler.dart';
 import 'package:moteelz/core/networking/errors/api_error_model.dart';
+import 'package:moteelz/features/wallet/data/models/country/country_model.dart';
 import 'package:moteelz/features/wallet/data/models/wallet/wallet_model.dart';
 import 'package:moteelz/features/wallet/data/repos/wallets_repo.dart';
 
@@ -19,6 +20,20 @@ class WalletsRepoImpl implements WalletsRepo {
         wallet.add(WalletModel.fromJson(item));
       }
       return Right(wallet);
+    } on ServerException catch (e) {
+      return Left(e.apiErrorModel);
+    }
+  }
+
+  @override
+  Future<Either<ApiErrorModel, List<CountryModel>>> getCountries() async {
+    try {
+      final response = await api.get(EndPoints.countries);
+      List<CountryModel> countries = [];
+      for (var item in response[ApiKeys.data]) {
+        countries.add(CountryModel.fromJson(item));
+      }
+      return Right(countries);
     } on ServerException catch (e) {
       return Left(e.apiErrorModel);
     }
